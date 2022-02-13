58caf1b78d98f01f4515303a4148f485061732ed
Evtim Georgiev Lyonov <elyon@softserveinc.com>
2022-02-13 17:45:46 +0200

    Images fixed

--- src/components/BasicProjects.tsx
+++ src/components/BasicProjects.tsx
@@ -2,49 +2,49 @@ const data = [
   {
     id: 1,
     name: 'Birthday Reminder',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/birthday.jpg',
+    img: './images/1.jpg',
     link: 'https://demo-birthday-reminder.netlify.app/',
   },
   {
     id: 2,
     name: 'Tours',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/tours.jpg',
+    img: './images/2.jpg',
     link: 'https://demo-tours-project.netlify.app/',
   },
   {
     id: 3,
     name: 'Reviews',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/reviews.jpg',
+    img: './images/3.jpg',
     link: 'https://demo-reviews.netlify.app/',
   },
   {
     id: 4,
     name: 'Accordion',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/acco.jpg',
+    img: './images/4.jpg',
     link: 'https://demo-accordion.netlify.app/',
   },
   {
     id: 5,
     name: 'Menu',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/menu.jpg',
+    img: './images/5.jpg',
     link: 'https://demo-menu-project.netlify.app/',
   },
   {
     id: 6,
     name: 'Tabs',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/exp.jpg',
+    img: './images/6.jpg',
     link: 'https://demo-tabs-project.netlify.app/',
   },
   {
     id: 7,
     name: 'Slider',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/reviews.jpg',
+    img: './images/7.jpg',
     link: 'https://demo-slider-project.netlify.app/',
   },
   {
     id: 8,
     name: 'Lorem Ipsum',
-    img: 'https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/lorem.jpg',
+    img: './images/8.jpg',
     link: 'https://demo-lorem-project.netlify.app/',
   },
 ];
@@ -64,8 +64,9 @@ export const BasicProjects = () => {
               className='bg-white transition duration-500 hover:scale-110 shadow-xl rounded overflow-hidden mb-8'>
               {console.log(el.img)}
               <a href={el.link} target='_blank' rel='noreferrer'>
-                <div
-                  className={`h-60 w-80 bg-cover bg-[url("https://raw.githubusercontent.com/evtimlyonov/Projects/master/public/images/birthday.jpg")]`}></div>
+                <div className={`h-60 w-80 bg-cover`}>
+                  <img src={el.img} alt={el.name} />
+                </div>
                 <div className='text-center py-4 px-5 bg-white'>
                   <h5>{el.name}</h5>
                 </div>
